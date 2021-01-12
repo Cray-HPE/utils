08a38c36a54e7713feb6e86c3dc4cd7794552245
Bob Gross <rgross@cray.com>
2021-01-12 12:40:08 -0600

    removed vault etcd checks

---
+++ ncnHealthChecks.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# Copyright 2020-2021 Hewlett Packard Enterprise Development LP
+#
+# The ncnHealthChecks script executes a number of NCN system health checks:
+#    Report Cray release info
+#    Report Kubernetes status for Master and worker nodes
+#    Report Ceph health status
+#    Report health of Etcd clusters in services namespace
+#    Report the number of pods on which worker node for each Etcd cluster
+#    List automated Etcd backups for BOS, BSS, CRUS DNS and
+#    Slingshot Controlleres
+#    Report ncn node uptimes
+#    Report worker ncn node pod counts
+#    Report pods yet to reach the running state
+#
+# Returned results are not verified. Information is provided to aide in
+# analysis of the results.
+#
+# The ncnHealthChecks script can be run on any ncn node from any directory. 
+# The ncnHealthChecks script can be run before and after an NCN has been
+# rebooted.
+#
+
+echo "             +++++ NCN Health Checks +++++"
+echo "=== Can Be Executed on any ncn node ==="
+hostName=$(hostname)
+echo "=== Executing on $hostName, $(date) ==="
+
+echo
+echo "=== Check Kubernetes' Master and Worker Node Status ==="
+echo "=== Verify Kubernetes' Node \"Ready\" Status and Version ==="
+date
+kubectl get nodes -o wide
+echo
+
+echo
+echo "=== Check Ceph Health Status ==="
+echo "=== Verify \"health: HEALTH_OK\" Status ==="
+echo "=== At times a status of HEALTH_WARN, too few PGs per OSD, and/or large \
+omap objects, may be okay ==="
+date
+ssh ncn-s001 ceph -s
+
+echo
+echo "=== Check the Health of the Etcd Clusters in the Services Namespace ==="
+echo "=== Verify a \"healthy\" Report for Each Etcd Pod. ==="
+date;
+for pod in $(kubectl get pods -l app=etcd -n services \
+		     -o jsonpath='{.items[*].metadata.name}')
+do
+    echo "### ${pod} ###"
+    kubectl -n services exec ${pod} -- /bin/sh -c "ETCDCTL_API=3 etcdctl endpoint health"
+done
+echo
+
+echo
+echo "=== Check the Number of Pods in Each Cluster. Verify they are Balanced. ==="
+echo "=== Each cluster should contain at least three pods, but may contain more. ==="
+echo "=== Ensure that no two pods in a given cluster exist on the same worker node. ==="
+date
+for ns in services
+do
+    for cluster in $(kubectl get etcdclusters.etcd.database.coreos.com \
+                             -n $ns | grep -v NAME | awk '{print $1}')
+    do
+        kubectl get pod -n $ns -o wide | grep $cluster; echo ""
+    done
+done
+
+echo
+echo "=== Check if any \"alarms\" are set for any of the Etcd Clusters in the \
+Services Namespace ==="
+echo "=== An empty list is returned if no alarms are set ==="
+for pod in $(kubectl get pods -l app=etcd -n services \
+                     -o jsonpath='{.items[*].metadata.name}')
+do
+    echo "### ${pod} Alarms Set: ###"
+    kubectl -n services exec ${pod} -- /bin/sh \
+            -c "ETCDCTL_API=3 etcdctl alarm list"
+done
+
+echo
+echo "=== Check the health of Etcd Cluster's database in the Services Namespace ==="
+echo "=== PASS or FAIL status returned ==="
+for pod in $(kubectl get pods -l app=etcd -n services \
+                     -o jsonpath='{.items[*].metadata.name}')
+do
+    echo "### ${pod} Etcd Database Check: ###"
+    dbc=$(kubectl -n services exec ${pod} -- /bin/sh \
+                  -c "ETCDCTL_API=3 etcdctl put foo fooCheck && \
+                  ETCDCTL_API=3 etcdctl get foo && \
+                  ETCDCTL_API=3 etcdctl del foo && \
+                  ETCDCTL_API=3 etcdctl get foo" 2>&1)
+    echo $dbc | awk '{ if ( $1=="OK" && $2=="foo" && \
+                       $3=="fooCheck" && $4=="1" && $5=="" ) print \
+    "PASS:  " PRINT $0; 
+    else \
+    print "FAILED DATABASE CHECK - EXPECTED: OK foo fooCheck 1 \
+    GOT: " PRINT $0 }'
+done
+
+echo
+echo "=== Etcd Clusters with Automatic Etcd Back-ups Configured: ==="
+echo "=== BOS, BSS, CRUS, DNS and Slingshot Controllers ==="
+echo "=== May want to ensure that automated back-ups are up to-date ==="
+echo "=== and that automated back-ups continue after NCN worker reboot. ==="
+echo "=== Clusters without Automated Backups: ==="
+echo "=== FAS, HBTD, HMNFD, REDS, UAS & CPS ==="
+date
+kubectl exec -it -n operators $(kubectl get pod -n operators | \
+grep etcd-backup-restore | head -1 | awk '{print $1}') -c boto3 -- list_backups ""
+date
+echo
+
+echo;
+echo "=== date; for h in ncn-w00{1,2,3} ncn-s00{1,2,3} ncn-m00{1,2,3}; do echo\
+ -n "$h: "; ssh \$h uptime; done ==="
+date; for h in ncn-w00{1,2,3} ncn-s00{1,2,3} ncn-m00{1,2,3}; \
+      do echo "$h:"; ssh $h uptime; done
+echo;
+
+echo;
+echo "=== date; kubectl get pods -A -o wide | grep -v Completed | grep w001 | wc -l ==="
+date; kubectl get pods -A -o wide | grep -v Completed | grep w001 | wc -l
+echo;
+echo "=== date; kubectl get pods -A -o wide | grep -v Completed | grep w002 | wc -l ==="
+date; kubectl get pods -A -o wide | grep -v Completed | grep w002 | wc -l
+echo;
+echo "=== date; kubectl get pods -A -o wide | grep -v Completed | grep w003 | wc -l ==="
+date; kubectl get pods -A -o wide | grep -v Completed | grep w003 | wc -l
+echo;
+
+echo
+echo "=== kubectl get pods -A -o wide | grep -v \"Completed\|Running\" ==="
+date
+kubectl get pods -A -o wide | grep -v "Completed\|Running"
+echo
+echo
---
+++ ncnPostgresHealthChecks.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Copyright 2020-2021 Hewlett Packard Enterprise Development LP
+#
+# For each postgres cluster, the ncnPostgresHealthChecks script determines
+# the leader pod and reports the status of all postgres pods in the cluster.
+#
+# Returned results are not verified.
+#
+# The ncnPostgresHealthChecks script can be run on any ncn node from any directory.
+#
+
+echo "             +++++ NCN Postgres Health Checks +++++";
+echo "=== Can Be Executed on any ncn node. ==="
+hostName=$(hostname)
+echo "=== Executing on $hostName, $(date) ==="
+
+echo
+echo "=== Postgresql Operator Version ==="
+kubectl get pod -n services -l "app.kubernetes.io/name=postgres-operator" \
+        -o jsonpath='{..image}' | xargs -n 1 | grep postgres | uniq
+
+echo
+echo "=== List of Postgresql Clusters Using Operator ==="
+kubectl get postgresql -A
+postgresClusters="$(kubectl get postgresql -A | awk '/postgres/ || NR==1' | \
+                    grep -v NAME | awk '{print $1","$2}')"
+
+echo
+echo "=== Look at patronictl list info for each cluster, determine and attach \
+to leader of each cluster ==="
+echo "=== Report status of postgres pods in cluster ==="
+
+dottedLine="------------------------------------------------------------------\
+--------------------------------"
+echo       "---${dottedLine}"
+echo
+
+for c in $postgresClusters
+do
+        # NameSpace and postgres cluster name
+        c_ns="$(echo $c | awk -F, '{print $1;}')"
+        c_name="$(echo $c | awk -F, '{print $2;}')"
+        # Get postgres pods for this cluster name:
+        members="$(kubectl get pod -n $c_ns -l "cluster-name=$c_name" \
+                           -o custom-columns=NAME:.metadata.name --no-headers)"
+        numMembers=$(echo "$members" | wc -l)
+        	
+	# Find the leader:
+        for m in $members
+        do
+            leader="$(kubectl -n $c_ns exec $m -- patronictl list 2>/dev/null \
+                      | awk ' $8 == "Leader" && $10 == "running" {print $4}')"
+            if [ -n "$leader" ]
+            then
+                break;
+            fi
+        done
+        if [ -z "$leader" ]
+        then
+            echo "=== ****** Did not find a leader for the $c_name cluster in \
+$numMembers pods ****** ==="
+            echo
+            kubectl get pods -A -o wide | grep "NAME\|$c_name"
+            other=$members
+        else
+            # Have a leader:
+            echo "=== Looking at patronictl list info for the $c_name cluster \
+with leader pod: $leader ==="
+            
+            other="$(echo $members | xargs -n 1 | grep -v $leader)"
+            
+            echo; echo "--- PSP for pod $leader ---"
+            kubectl -n $c_ns get pod $leader -o yaml | grep "kubernetes.io/psp"
+            
+            echo; echo "--- patronictl list for $c_ns leader pod $leader ---"
+            kubectl -n $c_ns exec $leader -- patronictl list 2>/dev/null
+            kubectl get pods -A -o wide | grep "NAME\|$c_name"
+            
+            echo; echo "--- Logs for $c_ns leader pod $leader ---"
+            kubectl logs -n $c_ns $leader postgres | \
+                awk '{$1="";$2=""; print $line}' | egrep "INFO|ERROR" \
+                | egrep -v "NewConnection|bootstrapping" | sort -u
+        fi
+        
+        for o in $other
+        do
+            echo; echo "--- Logs for $c_ns non-leader pod $o ---"
+            kubectl logs -n $c_ns $o postgres | awk '{$1="";$2=""; print $line}'\
+                | egrep "INFO|ERROR" | egrep -v "NewConnection|bootstrapping" \
+                | sort -u
+        done
+        echo;
+        echo $dottedLine
+        echo $dottedLine
+        echo
+done
+echo "=== kubectl get pods -A -o wide | grep \"NAME\|postgres-\" |\
+ grep -v \"operator\|Completed\" ==="
+echo
+kubectl get pods -A -o wide | grep "NAME\|postgres-" | grep -v "operator\|Completed"
+echo
+exit 0;
+
+
