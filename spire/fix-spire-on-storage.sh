744622effb0da3a65e634bf1b9ac7033952f2a1b
Kevin Burns <kevin.burns@hpe.com>
2023-06-12 16:16:28 -0500

    CASMPET-6643 Update to use new spire-server
    This updates the script to use the new spire server, cray-spire-server.
    This is used as part of the migration from 0.12.2 to 1.5.5

--- spire/fix-spire-on-storage.sh
+++ spire/fix-spire-on-storage.sh
@@ -28,20 +28,20 @@ RETRY=0
 MAX_RETRIES=30
 RETRY_SECONDS=10
 
-until kubectl exec -itn spire spire-server-0 --container spire-server -- ./bin/spire-server healthcheck | grep -q 'Server is healthy'; do
+until kubectl exec -itn spire cray-spire-server-0 --container spire-server -- ./bin/spire-server healthcheck | grep -q 'Server is healthy'; do
 	if [[ "$RETRY" -lt "$MAX_RETRIES" ]]; then
 		RETRY="$((RETRY + 1))"
-		echo "spire-server is not ready. Will retry after $RETRY_SECONDS seconds. ($RETRY/$MAX_RETRIES)"
+		echo "cray-spire-server is not ready. Will retry after $RETRY_SECONDS seconds. ($RETRY/$MAX_RETRIES)"
 	else
-		echo "spire-server did not start after $(echo "$RETRY_SECONDS" \* "$MAX_RETRIES" | bc) seconds."
+		echo "cray-spire-server did not start after $(echo "$RETRY_SECONDS" \* "$MAX_RETRIES" | bc) seconds."
 		exit 1
 	fi
 	sleep "$RETRY_SECONDS"
 done
 
-URL="https://spire-tokens.spire:54440/api/token"
-POD=$(kubectl get pods -n spire | grep spire-server | grep Running | awk 'NR==1{print $1}')
-LOADBALANCERIP=$(kubectl get service -n spire spire-lb --no-headers --output=jsonpath='{.spec.loadBalancerIP}')
+URL="https://cray-spire-tokens.spire:54440/api/token"
+POD=$(kubectl get pods -n spire | grep cray-spire-server | grep Running | awk 'NR==1{print $1}')
+LOADBALANCERIP=$(kubectl get service -n spire cray-spire-local --no-headers --output=jsonpath='{.spec.loadBalancerIP}')
 
 function sshnh() {
 	/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
@@ -76,10 +76,10 @@ for node in $nodes; do
 		fi
 		echo "$node is being joined to spire."
 		XNAME="$(sshnh "$node" cat /proc/cmdline | sed 's/.*xname=\([A-Za-z0-9]*\).*/\1/')"
-		TOKEN="$(kubectl exec -n spire "$POD" --container spire-registration-server -- curl -k -X POST -d type=storage\&xname="$XNAME" "$URL" | tr ':' '=' | tr -d '"{}')"
+		TOKEN="$(kubectl exec -n spire "$POD" --container registration-server -- curl -k -X POST -d type=storage\&xname="$XNAME" "$URL" | tr ':' '=' | tr -d '"{}')"
 		sshnh "$node" "echo $TOKEN > ${jointoken}"
-		kubectl get configmap -n spire spire-ncn-config -o jsonpath='{.data.spire-agent\.conf}' | sed "s/server_address.*/server_address = \"$LOADBALANCERIP\"/" | sshnh "$node" "cat > ${spireagent}"
-		kubectl get configmap -n spire spire-bundle -o jsonpath='{.data.bundle\.crt}' | sshnh "$node" "cat > ${spirebundle}"
+		kubectl get configmap -n spire cray-spire-ncn-config -o jsonpath='{.data.spire-agent\.conf}' | sed "s/server_address.*/server_address = \"$LOADBALANCERIP\"/" | sshnh "$node" "cat > ${spireagent}"
+		kubectl get configmap -n spire cray-spire-bundle -o jsonpath='{.data.bundle\.crt}' | sshnh "$node" "cat > ${spirebundle}"
 		sshnh "$node" systemctl enable spire-agent
 		sshnh "$node" systemctl start spire-agent
 	fi
